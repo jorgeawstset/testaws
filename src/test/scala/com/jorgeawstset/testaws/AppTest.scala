6ce28f9148aa3c28cf5023e82104838f1d8a185b
Jorge AWS Tset <jorgeawstset@gmail.com>
2020-08-20 23:26:38 +0200

    Testing UDF in Spark

--- src/test/scala/com/jorgeawstset/testaws/AppTest.scala
+++ src/test/scala/com/jorgeawstset/testaws/AppTest.scala
@@ -1,8 +1,8 @@
 package com.jorgeawstset.testaws
 
-import org.scalatest.FlatSpec
-
-class AppTest extends FlatSpec {
+//import org.scalatest.FlatSpec
+import org.scalatest.flatspec.AnyFlatSpec
+class AppTest extends AnyFlatSpec {
   "A String" should "have the same value" in {
     val cdena : String = new String("Hello World")
     assert( cdena === "Hello World")
